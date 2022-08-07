cba3d4f51d8b8d22878a1d2f7a78b8ad6beb7920
FelixEcker <felix0eckert0.online.de@gmail.com>
2022-08-07 17:50:01 +0200

    Infatry Primary

--- src/de/feckert/congame/common/troops/Infantry.java
+++ src/de/feckert/congame/common/troops/Infantry.java
@@ -1,6 +1,12 @@
 package de.feckert.congame.common.troops;
 
+import de.feckert.congame.util.ActionResult;
+
 public class Infantry extends Troop {
+    public static final float defenseBuff = .8f;
+    public int remainingRoundsPrimary = 0;
+    public int primaryCooldown = 0;
+
     public Infantry(int owner) {
         super(owner);
 
@@ -11,5 +17,24 @@ public class Infantry extends Troop {
         defDmgAbsorption = .35f;
         waterTravel = true;
         movement = 10;
+    }
+
+    @Override
+    public ActionResult primaryAction() {
+        if (primaryCooldown != 0 || remainingRoundsPrimary != 0) return ActionResult.FAILED;
+        primaryCooldown = 6;
+        remainingRoundsPrimary = 3;
+
+        defDmgAbsorption += defenseBuff;
+        dmgAbsorption += defenseBuff;
+
+        return ActionResult.SUCCESS;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        if (remainingRoundsPrimary != 0) remainingRoundsPrimary--;
+        if (primaryCooldown != 0 && remainingRoundsPrimary == 0) primaryCooldown--;
     }
 }
